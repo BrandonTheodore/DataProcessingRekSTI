eafd9084e1d4a012f88708e931df006c81c377f6
BrandonTheodore <18223020@std.stei.itb.ac.id>
2026-05-02 15:35:47 +0700

    feat: update preprocess and new endpoint

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from torchvision import transforms
 from PIL import Image
 import numpy as np
 import torch
@@ -14,17 +16,26 @@ latest_count = None
 
 def preprocess(image_bytes):
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-    img = np.array(image)
 
-    img = cv2.resize(img, (512, 512))
+    image.thumbnail((512, 512), Image.Resampling.LANCZOS)
 
-    img = img / 255.0
-    img = (img - [0.485, 0.456, 0.406]) / [0.229, 0.224, 0.225]
+    padded_image = Image.new('RGB', (512, 512), (0, 0, 0))
 
-    img = np.transpose(img, (2, 0, 1))
-    img = torch.tensor(img, dtype=torch.float32).unsqueeze(0)
+    paste_x = (512 - image.width) // 2
+    paste_y = (512 - image.height) // 2
+    padded_image.paste(image, (paste_x, paste_y))
 
-    return img
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], 
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    img_tensor = transform(padded_image).unsqueeze(0)
+
+    return img_tensor
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
@@ -54,3 +65,13 @@ def get_latest():
         "count": latest_count,
         "image_path": LATEST_IMAGE_PATH
     }
+
+@app.get("/latest-image")
+def get_latest_image():
+    if latest_count is None:
+        return {"message": "No image processed yet"}
+
+    return FileResponse(
+        LATEST_IMAGE_PATH,
+        media_type="image/jpeg"
+    )
